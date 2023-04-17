8b141253e64b4b16768c46c49153371ffff69b19
DalerKhusainov <dalerkhusainov85@gmail.com>
2023-04-17 23:51:54 +0500

    Started learning React Router

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <section className="section">
+      <h2>Home Page</h2>
+      <Link to="About" className="btn">
+        About
+      </Link>
+    </section>
+  );
+};
+export default Home;
